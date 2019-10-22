31c966a89e4a96f060c653a0061569911ca0ed11
Markus Dreyer <markus.a.dreyer@gmail.com>
2019-10-22 16:53:57 +0200

    fetching top albums progress

--- audiotaste/Controllers/ToplistViewController.swift
+++ audiotaste/Controllers/ToplistViewController.swift
@@ -11,4 +11,34 @@ import UIKit
 
 class ToplistViewController: UIViewController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        fetchTopAlbums()
+    }
+    
+    func fetchTopAlbums() {
+        
+        let urlSession = URLSession.shared
+        let url = URL.init(string:
+            "https://theaudiodb.com/api/v1/json/1/mostloved.php?format=album")!
+        
+        let task = urlSession.dataTask(with: url) { (data, response, error) in
+                   
+            if let data = data {
+                let responseString = String.init(data: data, encoding: String.Encoding.utf8)
+                       
+                let decoder = JSONDecoder.init()
+                       
+                //let albumObject = try! decoder.decode(Album.self, from: data)
+                
+                print(responseString)
+                //print(albumObject)
+                
+            } else if let error = error {
+                print(error)
+            }
+        }
+        task.resume()
+        
+    
+    }
 }
---
+++ audiotaste/Models/Album.swift
@@ -0,0 +1,48 @@
+//
+//  Album.swift
+//  audiotaste
+//
+//  Created by Markus Dreyer on 22/10/2019.
+//  Copyright © 2019 Markus Dreyer. All rights reserved.
+//
+
+import Foundation
+
+struct Album: Codable {
+    var idAlbum : String!
+    var idArtist : String!
+    var idLabel : String!
+    var intLoved : String!
+    var intSales : String!
+    var intScore : String!
+    var intScoreVotes : String!
+    var intYearReleased : String!
+    var strAlbum : String!
+    var strAlbum3DCase : String!
+    var strAlbumCDart : String!
+    var strAlbumSpine : String!
+    var strAlbumStripped : String!
+    var strAlbumThumb : String!
+    var strAlbumThumbBack : String!
+    var strAllMusicID : String!
+    var strArtist : String!
+    var strArtistStripped : String!
+    var strBBCReviewID : String!
+    var strDescription : String!
+    var strDescriptionPT : String!
+    var strDiscogsID : String!
+    var strGenre : String!
+    var strLabel : String!
+    var strLocked : String!
+    var strMood : String!
+    var strMusicBrainzArtistID : String!
+    var strMusicBrainzID : String!
+    var strRateYourMusicID : String!
+    var strReleaseFormat : String!
+    var strReview : String!
+    var strSpeed : String!
+    var strStyle : String!
+    var strWikidataID : String!
+    var strWikipediaID : String!
+}
+
