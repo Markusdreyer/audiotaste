6370302623eebe536ba44b3ed5d6134e616917f3
markusdreyer <markus.a.dreyer@gmail.com>
2019-12-03 16:14:36 +0100

    favorites progress

--- audiotaste/Controllers/FavoritesViewController.swift
+++ audiotaste/Controllers/FavoritesViewController.swift
@@ -9,6 +9,46 @@
 import Foundation
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    var albumData: [AlbumData] = []
+    var segueData: AlbumData!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
+    }
+
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     
+        let album = albumData[indexPath.row]
+    
+        self.segueData = album
+        performSegue(withIdentifier: "detailViewSegue", sender: self)
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albumData.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let album = albumData[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "toplistTableViewCell", for: indexPath) as! TableViewCell
+    
+    let imageUrl = URL(string: album.strAlbumThumb!)
+   
+    cell.tableAlbumLabel.text = album.strAlbumStripped
+    cell.tableArtistLabel.text = album.strArtist
+    cell.tableAlbumImageView.kf.setImage(with: imageUrl)
+    return cell
+    }
 }
