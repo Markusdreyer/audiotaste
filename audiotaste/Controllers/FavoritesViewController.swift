76ce25672c6f4d751862db4bc2b79b7fd55280c3
markusdreyer <markus.a.dreyer@gmail.com>
2019-12-04 16:58:39 +0100

    added edit functionality

--- audiotaste/Controllers/FavoritesViewController.swift
+++ audiotaste/Controllers/FavoritesViewController.swift
@@ -14,16 +14,14 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
     @IBOutlet weak var tableView: UITableView!
     var trackData = [String: [TrackData]]()
     var segueData: AlbumData!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fetchFavorites()
         self.tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.fetchFavorites()
-
     }
     
     func fetchFavorites() {
@@ -43,7 +41,27 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         }
         self.tableView.reloadData()
     }
-
+    
+    @IBAction func editButton(_ sender: UIBarButtonItem) {
+        self.tableView.isEditing = !self.tableView.isEditing
+        sender.title = (self.tableView.isEditing) ? "Done" : "Edit"
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let artistSection = Array(trackData.keys)[sourceIndexPath.section]
+        let tmp = trackData[artistSection]![sourceIndexPath.item]
+        trackData[artistSection]?.remove(at: sourceIndexPath.item)
+        trackData[artistSection]?.insert(tmp, at: destinationIndexPath.item)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == .delete) {
+             let artistSection = Array(trackData.keys)[indexPath.section]
+            trackData[artistSection]?.remove(at: indexPath.item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return trackData.keys.count
     }
@@ -54,9 +72,9 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let artists = Array(trackData.keys)
-        let sectionArtist = artists[section]
+        let artistSection = artists[section]
         
-        guard let sectionTracks = trackData[sectionArtist] else { return 0}
+        guard let sectionTracks = trackData[artistSection] else { return 0}
         
         return sectionTracks.count
     }
@@ -68,8 +86,8 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "favoritesTableViewCell", for: indexPath) as! FavoritesTableViewCell
         
-        let sectionArtist = Array(trackData.keys)[indexPath.section]
-        if let tracksInSection = trackData[sectionArtist] {
+        let artistSection = Array(trackData.keys)[indexPath.section]
+        if let tracksInSection = trackData[artistSection] {
             let duration = Int(tracksInSection[indexPath.row].intDuration!)
              cell.trackLabel.text = tracksInSection[indexPath.row].strTrack
              cell.runningTime.text = duration!.formatTime.minuteSeconds
